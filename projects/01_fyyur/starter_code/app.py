b8fa8419610633c857fd71b188c2d5cc4cd8b391
Dawei Zhang <dz4@pdx.edu>
2020-06-06 23:12:02 -0700

    add missing columns for Venue

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -39,8 +39,10 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    genres = db.Column(db.String(500))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
