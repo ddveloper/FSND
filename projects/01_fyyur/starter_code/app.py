d92f3eb9ae79be1dc619d6b82045648e441a5177
Dawei Zhang <dz4@pdx.edu>
2020-06-11 22:43:53 -0700

    finish 'edit' and 'delete'

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -243,12 +243,30 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  
+  error = 0
+  try:
+    db.session.query(Show).filter(Show.venue_id==venue_id).delete()
+    db.session.commit()
+    db.session.query(Venue).filter(Venue.id==venue_id).delete()
+    db.session.commit()
+  except:
+    error = 4
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+
+  if error:
+    flash('An error occurred. Venue {} could not be deleted.'.format(venue_id))
+  else:
+    flash('Venue {} was successfully deleted!'.format(venue_id))
+
+  return render_template('pages/home.html')
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  #return None
 
 #  Artists
 #  ----------------------------------------------------------------
@@ -335,54 +353,86 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+  artist=Artist.query.filter_by(id=artist_id).first()
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
+  
+  error = 0
+  try:
+    item = db.session.query(Artist).filter(Artist.id==artist_id).first()
 
-  return redirect(url_for('show_artist', artist_id=artist_id))
+    item.name = request.form['name']
+    item.city = request.form['city']
+    item.state = request.form['state']
+    item.phone = request.form['phone']
+    item.facebook_link = request.form['facebook_link']
+    item.genres = ','.join(request.form.getlist('genres'))
+    item.website = request.form['website']
+    item.image_link = request.form['image_link']
+    item.seeking_venue = 'seeking_venue' in request.form and request.form['seeking_venue'] == 'y'
+    item.seeking_description = request.form['seeking_description']
+
+    db.session.commit()
+  except:
+    error = 4
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+
+  if error:
+    flash('An error occurred. failed to update Artist ID = {}'.format(artist_id))
+  else:
+    flash('Artist ID = {} was successfully updated!'.format(artist_id))
+
+  if error:
+    return redirect(url_for('create_artist_submission'))
+  else:
+    return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.filter_by(id=venue_id).first()
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
+  error = 0
+  try:
+    item = db.session.query(Venue).filter(Venue.id==venue_id).first()
+
+    item.name = request.form['name']
+    item.city = request.form['city']
+    item.state = request.form['state']
+    item.address = request.form['address']
+    item.phone = request.form['phone']
+    item.image_link = request.form['image_link']
+    item.facebook_link = request.form['facebook_link']
+    item.website = request.form['website']
+    item.genres = ','.join(request.form.getlist('genres'))
+    item.seeking_talent = 'seeking_talent' in request.form and request.form['seeking_talent'] == 'y'
+    item.seeking_description = request.form['seeking_description']
+
+    db.session.commit()
+  except:
+    error = 4
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+
+  if error:
+    flash('An error occurred. failed to update Venue ID = {}'.format(venue_id))
+  else:
+    flash('Venue  ID = {} was successfully updated!'.format(venue_id))
+
+  if error:
+    return redirect(url_for('edit_venue_submission'))
+  else:
+    return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
