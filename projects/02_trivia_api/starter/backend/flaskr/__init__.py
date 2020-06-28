75606b2001d2fcb5ad38e660a235e9a76b7e5c3c
Dawei Zhang <dz4@pdx.edu>
2020-06-27 22:34:56 -0700

    [API] impl. endpoints 'questions' and 'categories'

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -12,21 +12,29 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
+  CORS(app)
   
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    return response
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
-
+  @app.route('/categories', methods=['GET'])
+  def get_categories():
+    categories = Category.query.all()
+    return jsonify({
+      'success': True,
+      'categories': {category.id: category.type for category in categories}
+    })
 
   '''
   @TODO: 
@@ -40,6 +48,21 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions', methods=['GET'])
+  def get_questions():
+    page = request.args.get('page', 1, type=int)
+    start = (page-1) * 10
+    end = start + 10
+    questions = Question.query.all()
+    formatted_questions = [question.format() for question in questions]
+    categories = Category.query.all()
+    return jsonify({
+      'success': True,
+      'questions': formatted_questions[start:end],
+      'total_questions': len(questions),
+      'current_category': None,
+      'categories': {category.id: category.type for category in categories}
+    })
 
   '''
   @TODO: 
